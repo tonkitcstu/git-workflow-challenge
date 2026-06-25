e124b79752f8dc0ff74ef6feab0537240efdba76
shane <jsonbrody5@gmail.com>
2026-06-25 15:40:53 +0700

    add logic of get_summary method

--- stock.py
+++ stock.py
@@ -82,6 +82,17 @@ class Stock:
         items: dict[str, int] = {}
 
         # YOUR CODE HERE.
+        for event in self.events:
+            if isinstance(event, ItemAdded):
+                if event.name in items:
+                    items[event.name] += 1
+                else:
+                    items[event.name] = 1
+            elif isinstance(event, ItemRemoved):
+                if event.name in items:
+                    items[event.name] -= 1
+                    if items[event.name] == 0:
+                        del items[event.name]
     
         return items
 

04f19cd998099dd3d417fc99c3963dc87a609aec
Kuntanat <kuntanat.sup@dome.tu.ac.th>
2026-06-25 15:46:49 +0700

    feat: add feature get_summary

--- stock.py
+++ stock.py
@@ -81,8 +81,18 @@ class Stock:
     def get_summary(self):
         items: dict[str, int] = {}
 
-        # YOUR CODE HERE.
-    
+        for event in self.events:
+            if isinstance(event, ItemAdded):
+                if event.name in items:
+                    items[event.name] += 1
+                else:
+                    items[event.name] = 1
+            elif isinstance(event, ItemRemoved):
+                if event.name in items:
+                    items[event.name] -= 1
+                    if items[event.name] == 0:
+                        del items[event.name]
+
         return items
 
     def print_summary(self):
